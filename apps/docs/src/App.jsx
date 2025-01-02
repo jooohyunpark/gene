ce4a27f24475d92d52fccd0ebba88f782a0a3ad2
Joohyun Park <jhp527@nyu.edu>
2025-01-02 14:37:41 -0500

    clean colors token

--- apps/docs/src/App.jsx
+++ apps/docs/src/App.jsx
@@ -1,7 +1,10 @@
 import { Link } from '@gene/ui';
+import { colors } from '@gene/tokens';
 import './App.css';
 
 function App() {
+  console.log(colors);
+
   return (
     <>
       <Link href="https://google.com">hello</Link>
