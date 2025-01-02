36ace0fd0d92214aefd8321d2c95e1f2da280cd5
Joohyun Park <jhp527@nyu.edu>
2025-01-02 15:22:10 -0500

    clean color

--- packages/tokens/color.ts
+++ packages/tokens/color.ts
@@ -1,8 +1,6 @@
 export const black = "#000000";
-export const blackHover = "#212121";
 
 export const white = "#ffffff";
-export const whiteHover = "#e8e8e8";
 
 export const blue10 = "#edf5ff";
 export const blue20 = "#d0e2ff";
@@ -25,29 +23,6 @@ export const blue = {
   80: blue80,
   90: blue90,
   100: blue100,
-};
-
-export const blue100Hover = "#001f75";
-export const blue90Hover = "#00258a";
-export const blue80Hover = "#0039c7";
-export const blue70Hover = "#0053ff";
-export const blue60Hover = "#0050e6";
-export const blue50Hover = "#1f70ff";
-export const blue40Hover = "#5c97ff";
-export const blue30Hover = "#8ab6ff";
-export const blue20Hover = "#b8d3ff";
-export const blue10Hover = "#dbebff";
-export const blueHover = {
-  10: blue10Hover,
-  20: blue20Hover,
-  30: blue30Hover,
-  40: blue40Hover,
-  50: blue50Hover,
-  60: blue60Hover,
-  70: blue70Hover,
-  80: blue80Hover,
-  90: blue90Hover,
-  100: blue100Hover,
 };
 
 export const gray10 = "#f4f4f4";
@@ -73,39 +48,9 @@ export const gray = {
   100: gray100,
 };
 
-export const gray10Hover = "#e8e8e8";
-export const gray20Hover = "#d1d1d1";
-export const gray30Hover = "#b5b5b5";
-export const gray40Hover = "#999999";
-export const gray50Hover = "#7a7a7a";
-export const gray60Hover = "#5e5e5e";
-export const gray70Hover = "#636363";
-export const gray80Hover = "#474747";
-export const gray90Hover = "#333333";
-export const gray100Hover = "#292929";
-export const grayHover = {
-  10: gray10Hover,
-  20: gray20Hover,
-  30: gray30Hover,
-  40: gray40Hover,
-  50: gray50Hover,
-  60: gray60Hover,
-  70: gray70Hover,
-  80: gray80Hover,
-  90: gray90Hover,
-  100: gray100Hover,
-};
-
 export const color = {
   black,
   blue,
   gray,
   white,
-
-  hover: {
-    black: blackHover,
-    blue: blueHover,
-    gray: grayHover,
-    white: whiteHover,
-  },
 };
