d06e457065a861b924cee7cde3e3a5408df32e7f
Joohyun Park <jhp527@nyu.edu>
2025-01-04 16:30:17 -0500

    add storybook theme

---
+++ packages/ui/.storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  colorPrimary: '#0f62fe',
+  brandTitle: 'Gene UI Storybook',
+  fontBase: `"Inter", "Helvetica Neue", sans-serif;`,
+});
