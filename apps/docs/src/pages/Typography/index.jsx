d107c20954bf49a8829080625b86903477cffbd0
Joohyun Park <jhp527@nyu.edu>
2025-01-08 10:38:19 -0500

    type scale table style update

--- apps/docs/src/pages/Typography/index.jsx
+++ apps/docs/src/pages/Typography/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from '@gene/ui';
 import Section from '@/components/Section';
-import { StyledTable, ExampleText } from './styles';
+import { StyledTable, ExampleText, TableLayout } from './styles';
 
 const Typography = () => (
   <>
@@ -23,139 +23,140 @@ const Typography = () => (
         screens.
       </p>
 
-      <StyledTable>
-        <caption>Type scale</caption>
-        <thead>
-          <tr>
-            <th scope="col">Type</th>
-            <th scope="col">Size (px/rem)</th>
-            <th scope="col">Line height</th>
-            <th scope="col">Weight</th>
-            <th scope="col">Letter spacing (em)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <ExampleText $type="heading01">Heading 01</ExampleText>
-            </td>
-            <td>48 / 3</td>
-            <td>1</td>
-            <td>600</td>
-            <td>-0.02</td>
-          </tr>
+      <TableLayout>
+        <StyledTable>
+          <thead>
+            <tr>
+              <th scope="col">Type</th>
+              <th scope="col">Size (px/rem)</th>
+              <th scope="col">Line height (unitless)</th>
+              <th scope="col">Weight</th>
+              <th scope="col">Letter spacing (em)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <ExampleText $type="heading01">Heading 01</ExampleText>
+              </td>
+              <td>48 / 3</td>
+              <td>1</td>
+              <td>600</td>
+              <td>-0.02</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="heading02">Heading 02</ExampleText>
-            </td>
-            <td>32 / 2</td>
-            <td>1</td>
-            <td>600</td>
-            <td>-0.02</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="heading02">Heading 02</ExampleText>
+              </td>
+              <td>32 / 2</td>
+              <td>1</td>
+              <td>600</td>
+              <td>-0.02</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="heading03">Heading 03</ExampleText>
-            </td>
-            <td>24 / 1.5</td>
-            <td>1.2</td>
-            <td>600</td>
-            <td>-0.01</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="heading03">Heading 03</ExampleText>
+              </td>
+              <td>24 / 1.5</td>
+              <td>1.2</td>
+              <td>600</td>
+              <td>-0.01</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="heading04">Heading 04</ExampleText>
-            </td>
-            <td>20 / 1.25</td>
-            <td>1.2</td>
-            <td>600</td>
-            <td>-0.01</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="heading04">Heading 04</ExampleText>
+              </td>
+              <td>20 / 1.25</td>
+              <td>1.2</td>
+              <td>600</td>
+              <td>-0.01</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="heading05">Heading 05</ExampleText>
-            </td>
-            <td>18 / 1.125</td>
-            <td>1.2</td>
-            <td>700</td>
-            <td>0</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="heading05">Heading 05</ExampleText>
+              </td>
+              <td>18 / 1.125</td>
+              <td>1.2</td>
+              <td>700</td>
+              <td>0</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="heading06">Heading 06</ExampleText>
-            </td>
-            <td>16 / 1</td>
-            <td>1.2</td>
-            <td>700</td>
-            <td>0</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="heading06">Heading 06</ExampleText>
+              </td>
+              <td>16 / 1</td>
+              <td>1.2</td>
+              <td>700</td>
+              <td>0</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="body01">Body 01</ExampleText>
-            </td>
-            <td>18 / 1.125</td>
-            <td>1.6</td>
-            <td>400</td>
-            <td>0</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="body01">Body 01</ExampleText>
+              </td>
+              <td>18 / 1.125</td>
+              <td>1.6</td>
+              <td>400</td>
+              <td>0</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="body02">Body 02</ExampleText>
-            </td>
-            <td>16 / 1</td>
-            <td>1.6</td>
-            <td>400</td>
-            <td>0</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="body02">Body 02</ExampleText>
+              </td>
+              <td>16 / 1</td>
+              <td>1.6</td>
+              <td>400</td>
+              <td>0</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="body03">Body 03</ExampleText>
-            </td>
-            <td>14 / .875</td>
-            <td>1.6</td>
-            <td>400</td>
-            <td>0</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="body03">Body 03</ExampleText>
+              </td>
+              <td>14 / .875</td>
+              <td>1.6</td>
+              <td>400</td>
+              <td>0</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="button01">Button 01</ExampleText>
-            </td>
-            <td>16 / 1</td>
-            <td>1.5</td>
-            <td>600</td>
-            <td>0</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="button01">Button 01</ExampleText>
+              </td>
+              <td>16 / 1</td>
+              <td>1.5</td>
+              <td>600</td>
+              <td>0</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="label01">Label 01</ExampleText>
-            </td>
-            <td>14 / .875</td>
-            <td>1.5</td>
-            <td>500</td>
-            <td>0.02</td>
-          </tr>
+            <tr>
+              <td>
+                <ExampleText $type="label01">Label 01</ExampleText>
+              </td>
+              <td>14 / .875</td>
+              <td>1.5</td>
+              <td>500</td>
+              <td>0.02</td>
+            </tr>
 
-          <tr>
-            <td>
-              <ExampleText $type="label02">Label 02</ExampleText>
-            </td>
-            <td>12 / .75</td>
-            <td>1.5</td>
-            <td>500</td>
-            <td>0.02</td>
-          </tr>
-        </tbody>
-      </StyledTable>
+            <tr>
+              <td>
+                <ExampleText $type="label02">Label 02</ExampleText>
+              </td>
+              <td>12 / .75</td>
+              <td>1.5</td>
+              <td>500</td>
+              <td>0.02</td>
+            </tr>
+          </tbody>
+        </StyledTable>
+      </TableLayout>
     </Section>
   </>
 );
--- apps/docs/src/pages/Typography/styles.js
+++ apps/docs/src/pages/Typography/styles.js
@@ -1,25 +1,36 @@
 import { color, space, typography } from '@gene/token';
 import styled from 'styled-components';
 
-export const StyledTable = styled.table`
-  max-width: 100%;
+export const TableLayout = styled.div`
   overflow-x: auto;
+  max-width: 100%;
+  margin-top: ${space(2)}px;
+`;
 
-  ${typography.body02};
+export const StyledTable = styled.table`
+  width: 100%;
+  table-layout: auto;
+  white-space: nowrap;
+  border-collapse: collapse;
 
-  th {
-    ${typography.body03};
-    font-weight: 700;
-  }
-
-  th,
-  td {
-    padding: ${space(1)}px ${space(2)}px;
-  }
+  ${typography.body03};
 
   thead {
     background-color: ${color.blue30};
     color: ${color.white};
+  }
+
+  th {
+    font-weight: 700;
+  }
+
+  tbody tr {
+    border-bottom: 1px solid ${color.gray60};
+  }
+
+  th,
+  td {
+    padding: ${space(1.5)}px ${space(3)}px;
   }
 `;
 
@@ -27,4 +38,5 @@ export const StyledThead = styled.thead``;
 
 export const ExampleText = styled.div`
   ${({ $type }) => typography[`${$type}`]};
+  white-space: nowrap;
 `;
