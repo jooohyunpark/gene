82b98443ab76669789866bbf36fb79cf61b3d44b
Joohyun Park <jhp527@nyu.edu>
2025-01-15 11:09:59 -0500

    design token docs wip

--- apps/docs/src/pages/foundation/DesignTokens/index.jsx
+++ apps/docs/src/pages/foundation/DesignTokens/index.jsx
@@ -1,11 +1,27 @@
-import { color } from '@gene/token';
+import { color, typography, space } from '@gene/token';
 import Section from '@/components/Section';
+
+console.log(color);
 
 const DesignTokens = () => (
   <>
     <Section>
       <h1>Design Tokens</h1>
-      <p>Documentation work in progress.</p>
+      <p>
+        Design tokens are design decisions, translated into data. GDS provides
+        primitive design tokens to ensure a cohesive experience.
+      </p>
+    </Section>
+
+    <Section>
+      <h2>Color</h2>
+      <div>
+        {Object.entries(color).map(([key, value], i) => (
+          <div key={i}>
+            color.{key} {value}
+          </div>
+        ))}
+      </div>
     </Section>
   </>
 );
