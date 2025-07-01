619bcc5f2c3e0e8fb844223a4a5ab4b5e1b96629
Joohyun Park <jhp527@nyu.edu>
2025-07-01 17:41:23 -0400

    migrate to pnpm

--- packages/token/rollup.config.mjs
+++ packages/token/rollup.config.mjs
@@ -1,31 +1,41 @@
 import path from 'path';
 import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
 
 // Resolve __dirname in ES modules
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default {
-  input: path.join(__dirname, 'src/index.ts'), // The entry point of your UI package
-  output: [
-    {
-      file: path.resolve(__dirname, 'dist/index.js'),
-      format: 'cjs', // CommonJS for Node.js compatibility
-      sourcemap: true,
-    },
-    {
-      file: path.resolve(__dirname, 'dist/index.esm.js'),
-      format: 'esm', // ES Module for bundlers like Webpack or Rollup
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve(), // Resolves dependencies from node_modules
-    commonjs(), // Converts CommonJS modules to ES6
-    typescript(),
-    terser(),
-  ],
-};
+const config = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: './dist/types',
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/types/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
+
+export default config;
